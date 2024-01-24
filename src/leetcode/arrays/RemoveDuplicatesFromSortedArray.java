62cfaeba6b8639e1cfb88ad061a1b269f24186b7
premsagarbhamidipati <spbham85@gmail.com>
2024-01-23 19:47:01 -0500

    feat: remove element leetcode Jan 23 2024

--- src/leetcode/arrays/RemoveDuplicatesFromSortedArray.java
+++ src/leetcode/arrays/RemoveDuplicatesFromSortedArray.java
@@ -14,6 +14,7 @@ public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the size of the array: ");
         int size = scanner.nextInt();
         int[] nums = new int[size];
         for (int i = 0; i < size; i++) {
@@ -25,15 +26,37 @@ public class RemoveDuplicatesFromSortedArray {
     private static int[] removeDuplicates(int[] nums) {
 
         // initialize a variable for previous number
-        int prev = 0;
+        int lftPntr = 0;
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int rtPntr = 1; rtPntr < nums.length; rtPntr++) {
 
-            if (nums[i]  != nums[prev]) {
-                prev++;
-                nums[prev] = nums[i];
+            if (nums[rtPntr]  != nums[lftPntr]) {
+                lftPntr++;
+                nums[lftPntr] = nums[rtPntr];
             }
         }
         return nums;
     }
+
+
+
+/*    class Solution {
+
+//nums[insertIndex] is left pointer, nums[i] is right pointer which is scanning through the array
+        public int removeDuplicates(int[] nums) {
+            int insertIndex = 1;
+            for(int i = 1; i < nums.length; i++){
+                // We skip to next index if we see a duplicate element
+                if(nums[i - 1] != nums[i]) {
+                *//* Storing the unique element at insertIndex index and incrementing
+                   the insertIndex by 1 *//*
+                    nums[insertIndex] = nums[i];
+
+                    // we increment the insertIndex after storing a unique element
+                    insertIndex++;
+                }
+            }
+            return insertIndex;
+        }
+    }*/
 }
---
+++ src/leetcode/arrays/RemoveElement.java
@@ -0,0 +1,38 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the array: ");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        int[] nums = new int[size];
+
+        for (int i=0; i < nums.length; i++) {
+            Scanner scanner1 = new Scanner(System.in);
+            int valAtIndex = scanner1.nextInt();
+            nums[i] = valAtIndex;
+        }
+
+        System.out.println("Enter value to remove from the array: ");
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        int k = removeElement(nums, val);
+        System.out.println("Output: " + k);
+    }
+
+    private static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        System.out.println("Array contains: "+ Arrays.toString(nums));
+        return i;
+    }
+}
